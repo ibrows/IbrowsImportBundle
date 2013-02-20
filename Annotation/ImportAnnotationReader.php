d8f53dddb811b0061bfd67e1b5e7866c8ab98d76
Mike Meier <mike.meier@ibrows.ch>
2013-02-20 21:42:55 +0100

    docs

--- Annotation/ImportAnnotationReader.php
+++ Annotation/ImportAnnotationReader.php
@@ -1,42 +1,50 @@
 <?php
 
 namespace Ibrows\ImportBundle\Annotation;
-use Ibrows\ImportBundle\Annotation\Mapping\MappingInterface;
-use Ibrows\ImportBundle\Annotation\Identifier\IdentifierInterface;
-use Ibrows\AnnotationReader\AnnotationReader;
 
+use Ibrows\ImportBundle\Annotation\Identifier\IdentifierInterface;
+use Ibrows\ImportBundle\Annotation\Mapping\MappingInterface;
+use Ibrows\ImportBundle\Annotation\Method\MethodInterface;
+use Ibrows\ImportBundle\Annotation\Compare\CompareInterface;
+
+use Ibrows\AnnotationReader\AnnotationReader;
 
 class ImportAnnotationReader extends AnnotationReader implements ImportAnnotationReaderInterface
 {
-
-    /* (non-PHPdoc)
-     * @see Ibrows\ImportBundle\Annotation.ImportAnnotationReaderInterface::getMappingAnnotations()
+    /**
+     * @param mixed $entity
+     * @return MappingInterface[]
      */
     public function getMappingAnnotations($entity)
     {
         return $this->getAnnotationsByType($entity, self::ANNOTATION_TYPE_MAPPING, self::SCOPE_PROPERTY);
     }
 
-    /* (non-PHPdoc)
-     * @see Ibrows\ImportBundle\Annotation.ImportAnnotationReaderInterface::getIdentifierAnnotations()
+    /**
+     * @param mixed $entity
+     * @return IdentifierInterface[]
      */
     public function getIdentifierAnnotations($entity)
     {
         return $this->getAnnotationsByType($entity, self::ANNOTATION_TYPE_IDENTIFIER, self::SCOPE_PROPERTY);
     }
 
-    /* (non-PHPdoc)
-     * @see Ibrows\ImportBundle\Annotation.ImportAnnotationReaderInterface::getMethodAnnotions()
+    /**
+     * @param mixed $entity
+     * @return MethodInterface[]
      */
     public function getMethodAnnotations($entity)
     {
         return $this->getAnnotationsByType($entity, self::ANNOTATION_TYPE_METHOD, self::SCOPE_METHOD);
     }
 
+    /**
+     * @param mixed $entity
+     * @return CompareInterface[]
+     */
     public function getCompareAnnotations($entity)
     {
        return $this->getAnnotationsByType($entity, self::ANNOTATION_TYPE_COMPARE, self::SCOPE_PROPERTY);
 
     }
-
 }
