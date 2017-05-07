39071316453fdea64eb572bb71cfa506581ee777
Stefan Vetsch <stefan.vetsch@pwc-digital.ch>
2017-05-07 16:36:36 +0200

    Add deprecations

--- Annotation/Compare/SimpleArray.php
+++ Annotation/Compare/SimpleArray.php
@@ -2,8 +2,11 @@
 
 namespace Ibrows\ImportBundle\Annotation\Compare;
 
+@trigger_error('The '.__NAMESPACE__.'\SimpleArray annotation is deprecated since version 1.3 and will be removed in 2.0. Use the Field annotation instead.', E_USER_DEPRECATED);
+
 /**
  * @Annotation
+ * @deprecated since version 1.3, to be removed in 2.0. Use the Field annotation instead
  */
 class SimpleArray extends AbstractCompare
 {
--- Annotation/Mapping/String.php
+++ Annotation/Mapping/String.php
@@ -2,8 +2,11 @@
 
 namespace Ibrows\ImportBundle\Annotation\Mapping;
 
+@trigger_error('The '.__NAMESPACE__.'\String annotation is deprecated since version 1.3 and will be removed in 2.0. Use the Field annotation instead.', E_USER_DEPRECATED);
+
 /**
  * @Annotation
+ * @deprecated since version 1.3, to be removed in 2.0. Use the Field annotation instead
  */
 class String extends AbstractMapping
 {
